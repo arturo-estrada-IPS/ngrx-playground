50d9fc317d151b9ea5a7e21aeee73dccc7e95b39
Arturo Estrada rodriguez <arturoe@mty.ipointsystems.com>
2020-03-12 19:07:11 -0600

    basic structure -in progress

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ShellComponent } from "./home/shell/shell.component";
+import { WelcomeComponent } from "./home/welcome/welcome.component";
+import { PageNotFoundComponent } from "./home/page-not-found/page-not-found.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ShellComponent,
+    children: [
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", pathMatch: "full", redirectTo: "welcome" }
+    ]
+  },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { ProductData } from "./products/product-data";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductModule } from "./products/product.module";
+import { UserModule } from "./user/user.module";
+import { SharedModule } from "./shared/shared.module";
+import { StoreModule } from "@ngrx/store";
+
+import { MenuComponent } from "./home/menu/menu.component";
+import { ShellComponent } from "./home/shell/shell.component";
+import { WelcomeComponent } from "./home/welcome/welcome.component";
+import { PageNotFoundComponent } from "./home/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ShellComponent,
+    WelcomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductModule,
+    UserModule,
+    SharedModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(ProductData),
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
